3be3d80b7ff2f61d900ff8b7c8f893d7a030509e
Gabriel <Gabrieldalves@outlook.com.br>
2022-01-06 23:38:24 -0300

    Implementação tabuleiro

--- src/br/com/gabriel/cm/modelo/Tabuleiro.java
+++ src/br/com/gabriel/cm/modelo/Tabuleiro.java
@@ -1,5 +1,46 @@
 package br.com.gabriel.cm.modelo;
 
+import java.util.ArrayList;
+import java.util.Iterator;
+import java.util.List;
+
 public class Tabuleiro {
+
+	private int linhas;
+	private int colunas;
+	private int minhas;
+
+	private final List<Campo> campos = new ArrayList<>();
+
+	public Tabuleiro(int linhas, int colunas, int minhas) {
+		this.linhas = linhas;
+		this.colunas = colunas;
+		this.minhas = minhas;
+
+		gerarCampos();
+		associarVizinhos();
+		sortearMinas();
+	}
+
+	private void gerarCampos() {
+		for (int linha = 0; linha < linhas; linha++) {
+			for (int coluna = 0; coluna < linhas; coluna++) {
+				campos.add(new Campo(linha, coluna));
+
+			}
+		}
+	}
+
+	private void associarVizinhos() {
+		for(Campo c1: campos) {
+			for(Campo c2: campos) {
+				c1.adicionarVizinho(c2);
+			}
+		}
+	}
+
+	private void sortearMinas() {
+
+	}
 
 }
